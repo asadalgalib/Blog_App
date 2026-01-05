47fc4026590791fe4e73442bc5e2a9be1f00cfe7
asadalgalib <asadalgalib9@gmail.com>
2026-01-05 17:57:54 +0600

    signup user added using better auth

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import { postRoutes } from './modules/post/route';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
+import cors from 'cors';
 // * Midlleware
 const app = express()
 app.use(express.json());
+app.use(cors({
+    origin: process.env.APP_URL || "http://localhost:5000",
+    credentials: true
+}))
+// * Better auth
+app.all('/api/auth/*splat', toNodeHandler(auth));
 // * Routes
 app.use("/posts", postRoutes);
 // * Default route
